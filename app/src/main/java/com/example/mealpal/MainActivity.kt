97fb9f73d550327a48e93551247668ce0e9449af
st10484119 <st10484119@vcconnect.edu.za>
2025-04-01 19:33:32 +0200

    first commit

---
+++ app/src/main/java/com/example/mealpal/MainActivity.kt
@@ -0,0 +1,100 @@
+package com.example.mealpal
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlin.system.exitProcess
+
+class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var Enter: EditText
+    private lateinit var ClrBtn: Button
+    private lateinit var ExtBtn: Button
+    private lateinit var GenBtn: Button
+    private lateinit var genTXT: TextView
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+
+        ClrBtn = findViewById(R.id.ClrBtn)
+        ExtBtn = findViewById(R.id.ExtBtn)
+        GenBtn = findViewById(R.id.GenBtn)
+        Enter = findViewById(R.id.Enter)
+        genTXT = findViewById(R.id.genTxt)
+
+
+        ClrBtn.setOnClickListener() {
+            Enter.text.clear()
+            genTXT.text = ""
+
+        }
+
+        ExtBtn.setOnClickListener() {
+            finishAffinity()
+            exitProcess(0)
+        }
+
+        GenBtn.setOnClickListener() {
+            suggestfood()
+
+
+        }
+    }
+
+
+    private fun suggestfood() {
+        val timeOfDay = Enter.text.toString().trim()
+        if (IsNotEmpty())
+
+        when  (timeOfDay.toString().trim()) {
+            "Breakfast" -> genTXT?.text = "Eggs \n \n Cereal \n \n Toast"
+            "Morning Snack" -> genTXT?.text = "Sandwich \n \n Muffin \n \n Apple"
+            "Lunch" -> genTXT?.text = "Sandwiches \n \n Ramen \n \n salad"
+            "Snack" -> genTXT?.text = "Chips \n \n Nuts \n \n Popcorn"
+            "Supper" -> genTXT?.text = "Butter Chicken \n \n Pasta \n \n Biryani"
+            "Dessert" -> genTXT?.text = "Cake \n \n Ice-Cream  \n \n Mulva Pudding"
+            "breakfast" -> genTXT?.text = "Eggs \n \n Cereal \n \n Toast"
+            "morning snack" -> genTXT?.text = "Sandwich \n \n Muffin \n \n Apple"
+            "lunch" -> genTXT?.text = "Sandwiches \n \n Ramen \n \n salad"
+            "snack" -> genTXT?.text = "Chips \n \n Nuts \n \n Popcorn"
+            "supper" -> genTXT?.text = "Butter Chicken \n \n Pasta \n \n Biryani"
+            "dessert" -> genTXT?.text = "Cake \n \n Ice-Cream  \n \n Mulva Pudding"
+            "dinner" -> genTXT?.text = "Butter Chicken \n \n Pasta \n \n Biryani"
+            "Dinner" -> genTXT?.text = "Butter Chicken \n \n Pasta \n \n Biryani"
+
+
+
+
+
+            else -> genTXT?.text = "Input Is Invalid"
+
+
+
+        }
+
+
+    }
+
+    private fun IsNotEmpty(): Boolean {
+        var b = true
+        if (Enter?.text.toString().trim().isEmpty()){
+            Enter?.error = "Input Required"
+        }
+        return b
+    }
+}
+
+
+
